13c3a166e76f1a3e2507e820388efdbba1ea4678
Brody14 <stefania.spinas@gmail.com>
2023-02-17 12:27:31 +0100

    FEAT vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Milos Teodosic')
+console.log('Milos Teodosic');
+
+
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+
